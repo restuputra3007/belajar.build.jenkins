38fc4a8fb259175360f875405057cd39c92136c8
restuputra3007 <res2putranto@gmail.com>
2022-11-06 21:42:56 +0700

    push project

---
+++ belajar.build.jenkins/src/main/java/belajar/build/jenkins/App.java
@@ -0,0 +1,13 @@
+package belajar.build.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World boys!" );
+    }
+}
